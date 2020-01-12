2deb9d6bf63010373975a36cdb461d98a386c25d
Kai Raphahn <kai.raphahn@laburec.de>
2020-01-12 03:02:28 +0100

    feat: Create file output
    finish files
    
    Issue #2

--- tests/logging/file.py
+++ tests/logging/file.py
@@ -16,14 +16,39 @@
 #    Copyright (C) 2017, Kai Raphahn <kai.raphahn@laburec.de>
 #
 
-import time
-import sys
 import os
 import unittest
 import unittest.mock as mock
 
 from bbutil.logging.writer.file import FileWriter
 from bbutil.logging.types import Message, Progress, Writer
+
+mocked_open1 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_open2 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_open2.side_effect = OSError(5)
+
+mocked_open3 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_open3.side_effect = ValueError(5)
+
+mocked_open4 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_open5 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_open6 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+
+mocked_write1 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write2 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write3 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write4 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write5 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write6 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+mocked_write7 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+
+mocked_close1 = unittest.mock.mock_open(read_data='file contents\nas needed\n')
+
+# mocked_open_fail1 = unittest.mock.mock_open()
+# mocked_open_fail1.side_effect = OSError(5)
+#
+# mocked_open_fail2 = unittest.mock.mock_open()
+# mocked_open_fail2.side_effect = ValueError("Invalid param")
 
 
 class Callback(object):
@@ -107,15 +132,7 @@ class TestConsoleWriter(unittest.TestCase):
         item.clear()
         return
 
-    mocked_open = unittest.mock.mock_open(read_data='file contents\nas needed\n')
-
-    mocked_open_fail1 = unittest.mock.mock_open()
-    mocked_open_fail1.side_effect = OSError(5)
-
-    mocked_open_fail2 = unittest.mock.mock_open()
-    mocked_open_fail2.side_effect = ValueError("Invalid param")
-
-    @mock.patch('builtins.open', new=mocked_open)
+    @mock.patch('builtins.open', new=mocked_open1)
     def test_open_01(self):
 
         item = FileWriter()
@@ -127,7 +144,7 @@ class TestConsoleWriter(unittest.TestCase):
         self.assertTrue(check)
         return
 
-    @mock.patch('builtins.open', new=mocked_open_fail1)
+    @mock.patch('builtins.open', new=mocked_open2)
     def test_open_02(self):
         item = FileWriter()
         item.setup(text_space=20, filename="tests.log", append_data=True)
@@ -138,7 +155,7 @@ class TestConsoleWriter(unittest.TestCase):
         self.assertFalse(check)
         return
 
-    @mock.patch('builtins.open', new=mocked_open_fail2)
+    @mock.patch('builtins.open', new=mocked_open3)
     def test_open_03(self):
         item = FileWriter()
         item.setup(text_space=20, filename="tests.log", append_data=True)
@@ -154,4 +171,182 @@ class TestConsoleWriter(unittest.TestCase):
         item.setup(text_space=20)
 
         self.assertRaises(ValueError, item.open)
+        return
+
+    @mock.patch('builtins.open', new=mocked_close1)
+    def test_close_01(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        check1 = item.open()
+        check2 = item.close()
+
+        self.assertTrue(check1)
+        self.assertTrue(check2)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write1)
+    def test_write_01(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        item.open()
+
+        m = Message(app="TEST", tag="RUN", level="INFORM", content="This is a test!")
+        item.write(m)
+
+        # noinspection PyUnresolvedReferences
+        write_called = item.file.write.called
+
+        # noinspection PyUnresolvedReferences
+        flush_called = item.file.flush.called
+
+        # noinspection PyUnresolvedReferences
+        call = item.file.write.call_args_list[0]
+        (args, kwargs) = call
+        data = str(args[0])
+
+        self.assertTrue(write_called)
+        self.assertTrue(flush_called)
+        self.assertIn(m.app, data)
+        self.assertIn(m.tag, data)
+        self.assertIn(m.level, data)
+        self.assertIn(m.content, data)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write2)
+    def test_write_02(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        item.open()
+
+        m = Message(app="TEST", level="INFORM", content="This is a test!")
+        item.write(m)
+
+        # noinspection PyUnresolvedReferences
+        write_called = item.file.write.called
+
+        # noinspection PyUnresolvedReferences
+        flush_called = item.file.flush.called
+
+        # noinspection PyUnresolvedReferences
+        call = item.file.write.call_args_list[0]
+        (args, kwargs) = call
+        data = str(args[0])
+
+        self.assertTrue(write_called)
+        self.assertTrue(flush_called)
+        self.assertIn(m.app, data)
+        self.assertIn(m.level, data)
+        self.assertIn(m.content, data)
+        return
+
+    def test_write_03(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        m = Message(app="TEST", tag="RUN", level="INFORM", content="This is a test!")
+        self.assertRaises(ValueError, item.write, m)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write4)
+    def test_write_04(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        item.open()
+
+        item.file.write.side_effect = OSError
+
+        m = Message(app="TEST", tag="RUN", level="INFORM", content="This is a test!")
+        item.write(m)
+
+        # noinspection PyUnresolvedReferences
+        write_called = item.file.write.called
+
+        # noinspection PyUnresolvedReferences
+        flush_called = item.file.flush.called
+
+        # noinspection PyUnresolvedReferences
+        call = item.file.write.call_args_list[0]
+        (args, kwargs) = call
+        data = str(args[0])
+
+        self.assertTrue(write_called)
+        self.assertFalse(flush_called)
+        self.assertIn(m.app, data)
+        self.assertIn(m.level, data)
+        self.assertIn(m.content, data)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write5)
+    def test_write_05(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        item.open()
+
+        m = Message(content="This is a test!", raw=True)
+        item.write(m)
+
+        # noinspection PyUnresolvedReferences
+        write_called = item.file.write.called
+
+        # noinspection PyUnresolvedReferences
+        flush_called = item.file.flush.called
+
+        # noinspection PyUnresolvedReferences
+        call = item.file.write.call_args_list[0]
+        (args, kwargs) = call
+        data = args[0].decode("utf-8")
+
+        self.assertTrue(write_called)
+        self.assertTrue(flush_called)
+        self.assertEqual(m.content, data)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write6)
+    def test_write_06(self):
+
+        item = FileWriter()
+        item.setup(text_space=20, filename="tests.log", append_data=True)
+
+        item.open()
+        item.file.write.side_effect = OSError
+
+        m = Message(content="This is a test!", raw=True)
+        item.write(m)
+
+        # noinspection PyUnresolvedReferences
+        write_called = item.file.write.called
+
+        # noinspection PyUnresolvedReferences
+        flush_called = item.file.flush.called
+
+        # noinspection PyUnresolvedReferences
+        call = item.file.write.call_args_list[0]
+        (args, kwargs) = call
+        data = args[0].decode("utf-8")
+
+        self.assertTrue(write_called)
+        self.assertFalse(flush_called)
+        self.assertEqual(m.content, data)
+        return
+
+    @mock.patch('builtins.open', new=mocked_write7)
+    def test_write_07(self):
+
+        item = FileWriter()
+        item.setup(text_space=20)
+
+        m = Message(content="This is a test!", raw=True)
+
+        self.assertRaises(ValueError, item.write, m)
         return
