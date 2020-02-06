3d172cd2a4858053c20420dcfa8214796ff1b8af
Kai Raphahn <kai.raphahn@laburec.de>
2020-02-06 22:41:58 +0100

    bug: Fix coverage bug in run-tests.py
    bump version to 0.2
    
    Issue #12

--- bbutil/__init__.py
+++ bbutil/__init__.py
@@ -51,13 +51,13 @@ __credits__ = [__author__]
 __milestone__ = 0
 
 #: version major
-__major__ = 1
+__major__ = 2
 
 #: version minor
 __minor__ = 0
 
 #: version patch
-__patch__ = 4
+__patch__ = 0
 
 #: package version
 __version__ = "{0:d}.{1:d}.{2:d}.{3:d}".format(__milestone__, __major__, __minor__, __patch__)
