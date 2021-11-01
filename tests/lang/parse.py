767ee0630c3db33d16c50dd28e21094c32067a33
Kai Raphahn <kai.raphahn@laburec.de>
2021-11-01 17:03:46 +0100

    feat: Add some tools to work with gettext
    test update
    
    Issue #16

--- tests/lang/parse.py
+++ tests/lang/parse.py
@@ -391,3 +391,78 @@ class TestParser(unittest.TestCase):
         self.assertEqual(_parser.length, 4, "_parser.length != 4")
         self.assertListEqual(_lines, _parser.script_line)
         return
+
+    # noinspection PyUnresolvedReferences
+    def test_update_01(self):
+        _locales = full_path("tests/locales")
+
+        _root = os.getcwd()
+        _testdata = full_path("{0:s}/testdata".format(_root))
+        sys.path.append(_root)
+
+        _package = full_path("{0:s}/testlang".format(_testdata))
+
+        _parser = Parser()
+        _check1 = _parser.setup(locales=_locales,
+                                module="testlang",
+                                filter="testlang.test1",
+                                package_path=_package)
+
+        _check2 = _parser.parse()
+        _parser.update()
+
+        _pot = full_path("{0:s}/.locales/gui/gui.pot".format(_root))
+        _po_en = full_path("{0:s}/tests/locales/en/LC_MESSAGES/gui.po".format(_root))
+        _po_de = full_path("{0:s}/tests/locales/de/LC_MESSAGES/gui.po".format(_root))
+
+        _lines = [
+            'echo "Update en/gui"',
+            'msgmerge -N -U {0:s} {1:s}'.format(_po_en, _pot),
+            'echo "Update de/gui"',
+            'msgmerge -N -U {0:s} {1:s}'.format(_po_de, _pot),
+        ]
+
+        self.assertNotEqual(_parser, None, "_parser: None")
+        self.assertEqual(_check1, True, "_check1 != True")
+        self.assertEqual(_check2, True, "_check2 != True")
+        self.assertEqual(_parser.length, 4, "_parser.length != 4")
+        self.assertListEqual(_lines, _parser.script_line)
+        return
+
+    # noinspection PyUnresolvedReferences
+    def test_update_02(self):
+        _locales = full_path("tests/locales")
+
+        _root = os.getcwd()
+        _testdata = full_path("{0:s}/testdata".format(_root))
+        sys.path.append(_root)
+
+        _package = full_path("{0:s}/testlang".format(_testdata))
+
+        _parser = Parser()
+        _check1 = _parser.setup(locales=_locales,
+                                windows=True,
+                                module="testlang",
+                                filter="testlang.test1",
+                                package_path=_package)
+
+        _check2 = _parser.parse()
+        _parser.update()
+
+        _pot = full_path("{0:s}/.locales/gui/gui.pot".format(_root))
+        _po_en = full_path("{0:s}/tests/locales/en/LC_MESSAGES/gui.po".format(_root))
+        _po_de = full_path("{0:s}/tests/locales/de/LC_MESSAGES/gui.po".format(_root))
+
+        _lines = [
+            'echo Update en/gui',
+            'msgmerge.exe -N -U {0:s} {1:s}'.format(_po_en, _pot),
+            'echo Update de/gui',
+            'msgmerge.exe -N -U {0:s} {1:s}'.format(_po_de, _pot),
+        ]
+
+        self.assertNotEqual(_parser, None, "_parser: None")
+        self.assertEqual(_check1, True, "_check1 != True")
+        self.assertEqual(_check2, True, "_check2 != True")
+        self.assertEqual(_parser.length, 4, "_parser.length != 4")
+        self.assertListEqual(_lines, _parser.script_line)
+        return
