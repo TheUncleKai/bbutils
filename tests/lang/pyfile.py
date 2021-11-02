abf0e720f34c7b990650a0262a2b30caf5c7389d
Kai Raphahn <kai.raphahn@laburec.de>
2021-11-02 16:20:51 +0100

    feat: Add some tools to work with gettext
    fix static paths
    
    Issue #16

--- tests/lang/pyfile.py
+++ tests/lang/pyfile.py
@@ -86,8 +86,8 @@ class TestPythonFile(unittest.TestCase):
 
         self.assertNotEqual(_pyfile, None, "_pyfile: None")
         self.assertEqual(_check1, True, "_check != True")
-        self.assertEqual(_pyfile.path, '{0:s}/gui'.format(_locales))
-        self.assertEqual(_pyfile.pot, '{0:s}/gui/_testlang.test1.pot'.format(_locales))
+        self.assertEqual(_pyfile.path, full_path('{0:s}/gui'.format(_locales)))
+        self.assertEqual(_pyfile.pot, full_path('{0:s}/gui/_testlang.test1.pot'.format(_locales)))
         self.assertEqual(_pyfile.domain, 'gui')
         self.assertEqual(_pyfile.classname, 'testlang.test1')
         return
@@ -147,8 +147,8 @@ class TestPythonFile(unittest.TestCase):
 
         self.assertNotEqual(_pyfile, None, "_pyfile: None")
         self.assertEqual(_check1, True, "_check != True")
-        self.assertEqual(_pyfile.path, '{0:s}/gui'.format(_locales))
-        self.assertEqual(_pyfile.pot, '{0:s}/gui/_testos1.pot'.format(_locales))
+        self.assertEqual(_pyfile.path, full_path('{0:s}/gui'.format(_locales)))
+        self.assertEqual(_pyfile.pot, full_path('{0:s}/gui/_testos1.pot'.format(_locales)))
         self.assertEqual(_pyfile.domain, 'gui')
         self.assertEqual(_pyfile.classname, 'testos1')
         return
