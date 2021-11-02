b234e0d7fbc820d6e4a1c7cd3d6123bfa857fc08
Kai Raphahn <kai.raphahn@laburec.de>
2021-11-02 15:17:19 +0100

    feat: Add some tools to work with gettext
    redo main script to parser
    
    Issue #16

--- make-lang.py
+++ make-lang.py
@@ -18,115 +18,48 @@
 
 
 import sys
-import os
-import os.path
-
 import bbutil.lang.parser
 
 from bbutil.logging import Logging
-from bbutil.lang.parser import Parser
-from bbutil.utils import full_path
-
-from argparse import ArgumentParser
+from bbutil.lang.parser import Parser, Command
 
 log: Logging = Logging()
-
-_commands = [
-    "generate",
-    "update",
-    "merge",
-    "compile",
-    "all"
-]
-
-
-class Main(object):
-
-    def __init__(self):
-        self.argument_parser = ArgumentParser()
-
-
-        self.parser: Parser = Parser()
-
-        self.command: str = ""
-        self.root_path: str = ""
-        self.is_windows: bool = False
-        self.verbose: int = 0
-        self.script_ext: str = ""
-        self.first_line: str = ""
-
-        self.list_generate = []
-        self.list_update = []
-        self.list_copy = []
-        self.list_compile = []
-        return
-
-    def setup(self) -> bool:
-        os.environ["IGNORE_GETTEXT"] = "True"
-
-        options = self.argument_parser.parse_args()
-
-        if options.module is None:
-            log.error("Module name is missing!")
-            sys.exit(1)
-
-        if options.script is None:
-            log.error("Script name is missing!")
-            sys.exit(1)
-
-        if options.command is None:
-            log.error("Command name is missing!")
-            sys.exit(1)
-
-        self.parser.setup(windows=options.windows, root=options.root, module=options.module, locales=options.locales,
-                          script=options.script)
-
-        self.root_path = options.root
-        self.command = options.command
-        self.is_windows = options.windows
-
-        bbutil.lang.parser.set_log(log)
-
-        if self.is_windows is True:
-            self.script_ext = ".cmd"
-            self.first_line = "@echo off\n"
-        else:
-            self.script_ext = ".sh"
-            self.first_line = "#!/bin/bash\n"
-
-        log.setup(app="make-lang.py", level=options.verbose)
-
-        console = log.get_writer("console")
-        console.setup(text_space=12, error_index=["ERROR", "EXCEPTION"])
-        log.register(console)
-
-        check = log.open()
-        if check is False:
-            sys.exit(1)
-
-        log.inform("Command", self.command)
-        log.inform("Windows", str(self.is_windows))
-        log.inform("Ext", self.script_ext)
-        return True
-
-    def run(self) -> bool:
-        _check = self.parser.parse()
-        if _check is False:
-            return False
-
-
-
-        return True
 
 
 if __name__ == '__main__':
 
-    main = Main()
+    bbutil.lang.parser.set_log(log)
+    log.setup(app="make-lang.py", level=0)
 
-    ret = main.setup()
-    if ret is False:
+    console = log.get_writer("console")
+    console.setup(text_space=12, error_index=["ERROR", "EXCEPTION"])
+    log.register(console)
+    _check = log.open()
+    if _check is False:
         sys.exit(1)
 
-    ret = main.run()
-    if ret is False:
+    main = Parser()
+
+    _check = main.init(sys.argv[1:])
+    if _check is False:
+        sys.exit(1)
+
+    _check = main.parse()
+    if _check is False:
+        sys.exit(1)
+
+    _check = main.store(command=Command.generate)
+    if _check is False:
+        sys.exit(1)
+
+    _check = main.store(command=Command.update)
+    if _check is False:
+        sys.exit(1)
+
+    _check = main.store(command=Command.copy)
+    if _check is False:
+        sys.exit(1)
+
+    _check = main.store(command=Command.compile)
+    if _check is False:
         sys.exit(1)
