fcea0e252bdc28555a1f10bd6764cea2327f20b5
Kai Raphahn <kai.raphahn@laburec.de>
2021-11-03 11:56:32 +0100

    feat: Add some tools to work with gettext
    create some helper for packaging
    
    Issue #16

--- create-package.sh
+++ create-package.sh
@@ -1,4 +1,74 @@
 #!/bin/bash
 
-rm dist/*
-python setup.py sdist bdist_wheel
+f=upload.sh
+
+CURRENT=`pwd`
+PASSWORD_FILE=.user_data.sh
+
+error() {
+    RC=$?
+    if [ ! $RC -eq 0 ] ; then
+        echo -e "\e[1;31mERROR:\e[0;0m Error $? ${1:-no error message provided}";
+        exit ${$RC};
+    fi
+}
+
+
+inform() {
+    echo -e "\e[1;32mInfo:\e[0;0m ${1}";
+}
+
+
+warning() {
+    echo -e "\e[1;33m*** Warning:\e[0;0m ${1}";
+}
+
+create-package() {
+    inform "Create"
+
+    rm dist/*
+    python setup.py sdist bdist_wheel
+}
+
+upload-test() {
+    inform "Upload test"
+
+    if [ -z "$PASSWORD_FILE" ]
+    then
+        echo "Unable to find login data"
+        exit 1
+    fi
+    source $PASSWORD_FILE
+    inform "Use $USERNAME_TEST"
+}
+
+upload-dist() {
+    inform "Upload dist"
+
+    if [ -z "$PASSWORD_FILE" ]
+    then
+        echo "Unable to find login data"
+        exit 1
+    fi
+    source $PASSWORD_FILE
+    inform "Use $USERNAME_DIST"
+}
+
+
+case "$1" in
+    create)
+        create-package
+        ;;
+
+    test)
+        upload-test
+        ;;
+
+    dist)
+        upload-dist
+        ;;
+
+    *)
+        echo "Usage: $f ( create | test | dist )"
+        ;;
+esac
