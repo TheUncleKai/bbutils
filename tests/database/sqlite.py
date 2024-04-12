54afe91469b4934cb1161f935b688178eff7fa88
Kai Raphahn <kai.raphahn@laburec.de>
2024-04-12 17:54:50 +0200

    feat: Add ability to change database scheme
    add more tests for drop columns
    
    Issue #41

--- tests/database/sqlite.py
+++ tests/database/sqlite.py
@@ -560,6 +560,28 @@ class TestSQLite(unittest.TestCase):
         self._clean(_sqlite)
         return
 
+    @mock.patch('sqlite3.connect', new=get_sqlite_operational_error())
+    def test_drop_columns_03(self):
+        # The drop functionality needs a minimal version to work
+        _check = self.check_minmal_version(3, 35, 0)
+        if _check is False:
+            return
+
+        _sqlite = copy_sqlite(filename="test_add_columns.sqlite", path="testdata/database")
+        _sqlite.prepare()
+
+        _table = Table(name="tester01", sqlite=_sqlite)
+        _table.add_column(name="testid", data_type=Types.integer, unique=True)
+        _table.add_column(name="use_test", data_type=Types.bool)
+        _table.add_column(name="testname", data_type=Types.string)
+        _table.add_column(name="path", data_type=Types.string)
+
+        _check = _sqlite.drop_columns(_table.name, ["path"])
+        self.assertFalse(_check)
+
+        self._clean(_sqlite)
+        return
+
     def test_rename_table_01(self):
         _sqlite = copy_sqlite(filename="test_add_columns.sqlite", path="testdata/database")
         _sqlite.prepare()
